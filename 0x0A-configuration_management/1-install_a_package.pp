63632971a8d3fd3ca83845d4f6a95df898212711
ChristopherMak <makeketachristopher@gmail.com>
2024-04-22 09:47:32 -0700

    Updated 1-install_a_package.pp file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
-#Install flask version 2.1.0
-
-
-exec {'pip3 install flask':
-require => Exec['python-installed'],
-command => '/usr/bin/pip3 install flask==2.1.0'
+# Using Puppet, install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-
-exec {'python-installed':
-command => '/usr/bin/which python3'
+#
+# Using Puppet, install Werkzeug from pip3
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
